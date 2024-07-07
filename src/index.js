8874bf39feb90b946d721aa983f91a684311d930
João Correia <joaocorreiaprof@gmail.com>
2024-07-07 21:33:43 +0100

    Added function to hit the API, process JSON and console the user search region

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import "./style.css";
 
-console.log("working!");
+const searchButton = document.getElementById("searchButton");
+
+async function fetchWeather(searchTerm = "aveiro") {
+  let link = `https://api.weatherapi.com/v1/current.json?key=6faa5075dd9445cfb4c215331240207&q=${searchTerm}`;
+
+  try {
+    let response = await fetch(link, { mode: "cors" });
+    let data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error fetching the weather:", error);
+  }
+}
+
+fetchWeather();
+
+searchButton.addEventListener("click", () => {
+  const userInput = document.getElementById("search").value;
+  fetchWeather(userInput);
+});
